571181dffe7601a7f232e3a729b05e0f9b1dc679
hailtherosya <hailtherosya@gmail.com>
2023-12-11 05:09:06 +0300

    refactor

--- src/bot.py
+++ src/bot.py
@@ -18,43 +18,45 @@ async def delete(peer_id, message_id):
 
 
 async def on_startup():
-    buffer = []
     conversations = await bot.api.messages.get_conversations()
     for chat in conversations.items:
-        if chat.conversation.peer.id > 2000000000:
-            buffer.append(chat.conversation.peer.id)
-    for chat_peer_id in buffer:
-        members = await bot.api.messages.get_conversation_members(peer_id=chat_peer_id)
-        storage.add_chat(
-            chat_id=chat_peer_id, members_ids=list(map(lambda x: x.member_id, members.items))
-        )
+        if chat.conversation.peer.id < 2000000000:
+            continue
+        members = await bot.api.messages.get_conversation_members(
+            peer_id=chat.conversation.peer.id
+            )
+        map(lambda x: storage.add_id(x.member_id), members.items)
 
 
 @bot.on.chat_message()
 async def chat_message(message: Message):
-    if message.from_id in storage.muted:
-        current_time = time.time()
-        if current_time - storage.muted.get(message.from_id) > 60:
-            storage.muted.pop(message.from_id)
-            storage.add_member(message.peer_id, message.from_id)
-            return
-        await delete(message.peer_id, message.conversation_message_id)
-        return
-    for i in storage.chats:
-        if i.id == message.peer_id:
-            chat = i
-            break
-    if message.from_id not in list(map(lambda x: x.id, chat.members)):
-        storage.muted.update({message.from_id: time.time()})
-        await delete(message.peer_id, message.conversation_message_id)
+    user_id = message.from_id
+    peer_id = message.peer_id
+    cmid = message.conversation_message_id
+    if user_id in storage.trust_ids:
+        pass
+    elif user_id not in storage.muted_ids:
+        storage.muted_ids.update({user_id: time.time()})
+        await delete(peer_id, cmid)
         message_id = await bot.api.messages.send(
-            peer_id=message.peer_id,
+            peer_id=peer_id,
             random_id=random.getrandbits(128),
-            message="К нам присоединился новый пользователь. Добро пожаловать! В течении первой минуты общения, вы не можете писать сообщения. Просим прощения за неудобство",
+            message="К нам присоединился новый пользователь. Добро пожаловать! " +
+                    "В течении первой минуты общения, вы не можете писать сообщения. " +
+                    "Просим прощения за неудобство",
         )
         await asyncio.sleep(20)
-        await bot.api.messages.delete(peer_id=message.peer_id, message_ids=[message_id], delete_for_all=True)
-
+        await bot.api.messages.delete(
+            peer_id=peer_id,
+            message_ids=[message_id],
+            delete_for_all=True
+            )
+    else:
+        if time.time() - storage.muted_ids.get(user_id) > 60:
+            storage.muted_ids.pop(user_id)
+            storage.add_id(user_id=user_id)
+            return
+        await delete(peer_id, cmid)
 
 if __name__ == "__main__":
     bot.loop_wrapper.add_task(on_startup())
--- src/storage.py
+++ src/storage.py
@@ -1,26 +1,8 @@
 class Storage:
     def __init__(self):
-        self.chats: Chat = []
-        self.muted = {}
+        self.trust_ids: list[int] = []
+        self.muted_ids = {}
 
-    def add_chat(self, chat_id: int, members_ids: list[int]):
-        self.chats.append(Chat(chat_id=chat_id, members_ids=members_ids))
-
-    def add_member(self, chat_id: int, member_id: int):
-        for chat in self.chats:
-            if chat.id == chat_id:
-                chat.add_member(member_id)
-
-
-class Chat:
-    def __init__(self, chat_id: int, members_ids: list[int]):
-        self.id = chat_id
-        self.members: Member = list(map(lambda x: Member(x), members_ids))
-
-    def add_member(self, member_id: int):
-        self.members.append(Member(member_id=member_id))
-
-
-class Member:
-    def __init__(self, member_id: int):
-        self.id = member_id
+    def add_id(self, user_id: int):
+        self.trust_ids.append(user_id)
+        self.trust_ids = list(set(self.trust_ids))
